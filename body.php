6bdc3e889337013eba30936f7fe355abfcb7fcd3
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-04-14 03:11:08 +0700

    Fix error query mysqli in php
    Recommended: Please rename field `Job'Father` and `Job'Mother` to `JobFather` and `JobMother`!

--- body.php
+++ body.php
@@ -6,11 +6,11 @@
               <div class="wrapper count-title d-flex">
                 <div class="icon"><i class="icon-picture"></i></div>
                 <div class="name"><strong class="text-uppercase">CLASSROOM</strong><span>IS USING</span>
-                <?php 
+                <?php
                 $result = mysqli_query($conn," SELECT `ClassId`, `ClassName`, `Enrollment`, `ClassStatus`, `GradeId` FROM `Class` WHERE `ClassStatus`=1");
                 $num_rows = mysqli_num_rows($result);
                 ?>
-               
+
                   <div class="count-number"><?=$num_rows?></div>
                 </div>
               </div>
@@ -20,7 +20,7 @@
               <div class="wrapper count-title d-flex">
                 <div class="icon"><i class="icon-padnote"></i></div>
                 <div class="name"><strong class="text-uppercase">PERSONNEL</strong><span>TOTAL</span>
-                <?php 
+                <?php
                 $result1=mysqli_query($conn,"SELECT * FROM `Personnel` ");
                 $num_rows1 = mysqli_num_rows($result1);
                 ?>
@@ -33,8 +33,8 @@
               <div class="wrapper count-title d-flex">
                 <div class="icon"><i class="icon-user"></i></div>
                 <div class="name"><strong class="text-uppercase">STUDENT</strong><span>IS LEARNING</span>
-                <?php 
-                $result2 = mysqli_query($conn," SELECT `StudentCode`, `StudentName`, `StudentBirth`, `StudentGender`, `StudentAddress`, `YourFatherName`, `JobFather`, `YourMotherName`, `JobMother`, `PhoneHouse`, `StudentStatus`, `ClassId` FROM `Student` WHERE `StudentStatus`=1");
+                <?php
+                $result2 = mysqli_query($conn," SELECT * FROM `Student` WHERE `StudentStatus`=1");
                 $num_rows2 = mysqli_num_rows($result2);
                 ?>
                   <div class="count-number"><?=$num_rows2?></div>
@@ -46,7 +46,7 @@
               <div class="wrapper count-title d-flex">
                 <div class="icon"><i class="icon-bill"></i></div>
                 <div class="name"><strong class="text-uppercase">ROOM</strong><span>TOTAL</span>
-                <?php 
+                <?php
                 $result3 = mysqli_query($conn,"SELECT * FROM `Class`");
                 $num_rows3 = mysqli_num_rows($result3)
                 ?>
@@ -59,7 +59,7 @@
               <div class="wrapper count-title d-flex">
                 <div class="icon"><i class="icon-list"></i></div>
                 <div class="name"><strong class="text-uppercase">POSITION</strong><span>TOTAL</span>
-                <?php 
+                <?php
                 $result4 = mysqli_query($conn,"SELECT `PositionId`, `PositionName`, `PositionDetails`, `DepartmentId` FROM `Position`");
                 $num_rows4 = mysqli_num_rows($result4)
                 ?>
@@ -72,7 +72,7 @@
               <div class="wrapper count-title d-flex">
                 <div class="icon"><i class="icon-list-1"></i></div>
                 <div class="name"><strong class="text-uppercase">DEPARTMENT</strong><span>TOTAL</span>
-                <?php 
+                <?php
                 $result5 = mysqli_query($conn,"SELECT `DepartmentId`, `DepartmentName`, `DepartmentDetails` FROM `Department` ");
                 $num_rows5 = mysqli_num_rows($result5)
                 ?>
@@ -91,35 +91,35 @@
                 <h2 class="display h4">To do List</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                 <ul class="check-lists list-unstyled">
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-1" name="list-1" class="form-control-custom">
                     <label for="list-1">Similique sunt in culpa qui officia</label>
                   </li>
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-2" name="list-2" class="form-control-custom">
                     <label for="list-2">Ed ut perspiciatis unde omnis iste</label>
                   </li>
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-3" name="list-3" class="form-control-custom">
                     <label for="list-3">At vero eos et accusamus et iusto </label>
                   </li>
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-4" name="list-4" class="form-control-custom">
                     <label for="list-4">Explicabo Nemo ipsam voluptatem</label>
                   </li>
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-5" name="list-5" class="form-control-custom">
                     <label for="list-5">Similique sunt in culpa qui officia</label>
                   </li>
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-6" name="list-6" class="form-control-custom">
                     <label for="list-6">At vero eos et accusamus et iusto </label>
                   </li>
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-7" name="list-7" class="form-control-custom">
                     <label for="list-7">Similique sunt in culpa qui officia</label>
                   </li>
-                  <li class="d-flex align-items-center"> 
+                  <li class="d-flex align-items-center">
                     <input type="checkbox" id="list-8" name="list-8" class="form-control-custom">
                     <label for="list-8">Ed ut perspiciatis unde omnis iste</label>
                   </li>
@@ -205,7 +205,7 @@
                 <div id="updates-box" role="tabpanel" class="collapse show">
                   <ul class="news list-unstyled">
                     <!-- Item-->
-                    <li class="d-flex justify-content-between"> 
+                    <li class="d-flex justify-content-between">
                       <div class="left-col d-flex">
                         <div class="icon"><i class="icon-rss-feed"></i></div>
                         <div class="title"><strong>Lorem ipsum dolor sit amet.</strong>
@@ -217,7 +217,7 @@
                       </div>
                     </li>
                     <!-- Item-->
-                    <li class="d-flex justify-content-between"> 
+                    <li class="d-flex justify-content-between">
                       <div class="left-col d-flex">
                         <div class="icon"><i class="icon-rss-feed"></i></div>
                         <div class="title"><strong>Lorem ipsum dolor sit amet.</strong>
@@ -229,7 +229,7 @@
                       </div>
                     </li>
                     <!-- Item-->
-                    <li class="d-flex justify-content-between"> 
+                    <li class="d-flex justify-content-between">
                       <div class="left-col d-flex">
                         <div class="icon"><i class="icon-rss-feed"></i></div>
                         <div class="title"><strong>Lorem ipsum dolor sit amet.</strong>
@@ -241,7 +241,7 @@
                       </div>
                     </li>
                     <!-- Item-->
-                    <li class="d-flex justify-content-between"> 
+                    <li class="d-flex justify-content-between">
                       <div class="left-col d-flex">
                         <div class="icon"><i class="icon-rss-feed"></i></div>
                         <div class="title"><strong>Lorem ipsum dolor sit amet.</strong>
@@ -253,7 +253,7 @@
                       </div>
                     </li>
                     <!-- Item-->
-                    <li class="d-flex justify-content-between"> 
+                    <li class="d-flex justify-content-between">
                       <div class="left-col d-flex">
                         <div class="icon"><i class="icon-rss-feed"></i></div>
                         <div class="title"><strong>Lorem ipsum dolor sit amet.</strong>
@@ -265,7 +265,7 @@
                       </div>
                     </li>
                     <!-- Item-->
-                    <li class="d-flex justify-content-between"> 
+                    <li class="d-flex justify-content-between">
                       <div class="left-col d-flex">
                         <div class="icon"><i class="icon-rss-feed"></i></div>
                         <div class="title"><strong>Lorem ipsum dolor sit amet.</strong>
